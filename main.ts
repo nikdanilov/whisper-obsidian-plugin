68bca5336294e9da7b85500a51dff40ea8edcfb0
Sparsh Yadav <sparshy@gmail.com>
2023-04-30 17:18:45 +0530

    Added command and a recording status in the status bar resolves #6

--- main.ts
+++ main.ts
@@ -13,6 +13,8 @@ export default class Whisper extends Plugin {
     recorder: NativeAudioRecorder
     audioHandler: AudioHandler;
     controls: Controls | null = null;
+    statusBarItem: HTMLElement | null = null;
+
 
     async onload() {
         this.settingsManager = new SettingsManager(this);
@@ -30,11 +32,54 @@ export default class Whisper extends Plugin {
         this.timer = new Timer()
         this.audioHandler = new AudioHandler(this);
         this.recorder = new NativeAudioRecorder()
-    }
 
+
+        this.statusBarItem = this.addStatusBarItem();
+        this.updateStatusBarItem(false);
+        this.addCommands();
+    }
+    updateStatusBarItem(recording: boolean) {
+        if (this.statusBarItem) {
+            this.statusBarItem.textContent = recording ? "Recording..." : "";
+            this.statusBarItem.style.color = recording ? "red" : "";
+        }
+    }
     onunload() {
         if (this.controls) {
             this.controls.close();
         }
+
+
+        if (this.statusBarItem) {
+            this.statusBarItem.remove();
+        }
+    }
+
+    addCommands() {
+        let recording = false;
+
+        this.addCommand({
+            id: "start-stop-recording",
+            name: "Start/Stop recording",
+            callback: async () => {
+                if (!recording) {
+                    recording = true;
+                    await this.recorder.startRecording();
+                } else {
+                    recording = false;
+                    const audioBlob = await this.recorder.stopRecording();
+                    // Use audioBlob to send or save the recorded audio as needed
+                    await this.audioHandler.sendAudioData(audioBlob);
+                }
+                this.updateStatusBarItem(recording);
+
+            },
+            hotkeys: [
+                {
+                    modifiers: ["Alt"],
+                    key: "`",
+                },
+            ],
+        });
     }
 }
